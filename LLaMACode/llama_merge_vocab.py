aa4778f7446c742d88c9a9c9c5ec6322edbc66ae
Nandini Mundra <46605294+nandinimundra@users.noreply.github.com>
2023-08-29 21:11:56 +0530

    added en filter

--- LLaMACode/llama_merge_vocab.py
+++ LLaMACode/llama_merge_vocab.py
@@ -4,6 +4,7 @@ from transformers import LlamaTokenizer
 from sentencepiece import sentencepiece_model_pb2 as sp_pb2_model
 import sentencepiece as spm
 import argparse
+import re
 parser = argparse.ArgumentParser()
 parser.add_argument('--llama_tokenizer_dir', default="llama_fast_tokenizer", type=str)
 parser.add_argument('--indic_sp_model_file', default='/nlsasfs/home/ai4bharat/nandinim/nandini/vocab_adap/sp_indic.model', type=str)
@@ -32,22 +33,31 @@ print(llama_tokenizer.all_special_ids)
 print(llama_tokenizer.special_tokens_map)
 
 
+def not_contains_eng(text):
+	eng_pattern = re.compile(r'[\u0020-\u007E]+')  # Range for english characters
+	if eng_pattern.search(text):
+		return False
+	else:
+		return True
+
+
 ## Add indic tokens to LLaMA tokenizer
 llama_spm_tokens_set=set(p.piece for p in llama_spm.pieces)
 print(len(llama_spm_tokens_set))
 print(f"Before:{len(llama_spm_tokens_set)}")
 for p in indic_spm.pieces:
     piece = p.piece
-    if piece not in llama_spm_tokens_set:
+    if piece not in llama_spm_tokens_set and not_contains_eng(piece) :
         new_p = sp_pb2_model.ModelProto().SentencePiece()
         new_p.piece = piece
         new_p.score = 0
         llama_spm.pieces.append(new_p)
+    
 print(f"New model pieces: {len(llama_spm.pieces)}")
 
 ## Save
-output_sp_dir_m = 'indic_llama_sp_m'
-output_hf_dir_m = 'indic_llama_hf_m' # the path to save indic-LLaMA tokenizer
+output_sp_dir_m = 'indic_llama_sp_m_filter'
+output_hf_dir_m = 'indic_llama_hf_m_filter' # the path to save indic-LLaMA tokenizer
 os.makedirs(output_sp_dir_m,exist_ok=True)
 with open(output_sp_dir_m+'/indic_llama.model', 'wb') as f:
     f.write(llama_spm.SerializeToString())
